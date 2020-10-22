20429d7bc37861aa1be545c7e6d293eceae4b207
Mark Mauro <markymauro@gmail.com>
2020-10-21 23:21:17 -0400

    Initial commit

---
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -0,0 +1,25 @@
+package com.example.instagramclone;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+import java.io.PipedOutputStream;
+
+public class ParseApplication extends Application {
+
+    // Initializes Parse SDK as soon as the application is created
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+       // ParseObject.registerSubclass(Post.class); this line of code is not working. Post.class seems to be unrecognizeable
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("HCp9uApQ3sKsgkzYNGoRfXMhCYmXfVQPHInC06R9")
+                .clientKey("HX9izNn7SbvMO4exFak6BWstYEEGbm1nSBUJyit8")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
